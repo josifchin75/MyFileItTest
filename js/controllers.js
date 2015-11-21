4269428a720d48c79188f1d05377b6edd3f9e155
josifchin75 <josifchin75@gmail.com>
2015-11-20 20:58:03 -0500

    IONIC Build

---
+++ js/controllers.js
@@ -0,0 +1,102 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('scanDocumentsCtrl', function($scope) {
+
+})
+   
+.controller('viewYourDocumentsCtrl', function($scope) {
+
+})
+   
+.controller('inviteAFriendCtrl', function($scope) {
+
+})
+      
+.controller('mainCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+   
+.controller('forgotPasswordCtrl', function($scope) {
+
+})
+   
+.controller('logOutCtrl', function($scope) {
+
+})
+   
+.controller('sharingKeyManagementCtrl', function($scope) {
+
+})
+   
+.controller('editAccountLoginCtrl', function($scope) {
+
+})
+   
+.controller('editAccountUserCtrl', function($scope) {
+
+})
+   
+.controller('setUpTeamAndPlayersCtrl', function($scope) {
+
+})
+   
+.controller('scannerSettingsCtrl', function($scope) {
+
+})
+   
+.controller('newAccountCtrl', function($scope) {
+
+})
+   
+.controller('newAccountProfileCtrl', function($scope) {
+
+})
+   
+.controller('successCtrl', function($scope) {
+
+})
+   
+.controller('forgottenPasswordSentCtrl', function($scope) {
+
+})
+   
+.controller('confirmDocumentCtrl', function($scope) {
+
+})
+   
+.controller('uploadSuccessCtrl', function($scope) {
+
+})
+   
+.controller('errorHasOccurredCtrl', function($scope) {
+
+})
+   
+.controller('viewImagesCtrl', function($scope) {
+
+})
+      
+.controller('shareDocumentsCtrl', function($scope) {
+
+})
+   
+.controller('confirmDocumentShareCtrl', function($scope) {
+
+})
+   
+.controller('emergencyShareCtrl', function($scope) {
+
+})
+   
+.controller('shareHasBeenSentCtrl', function($scope) {
+
+})
+ 
